ee96f3ae4c4f0d3ee5efcc85a1680be109a3214d
sathwik.r <reddysathwik56@gmail.com>
2025-10-23 18:16:28 +0530

    FirstCommit

---
+++ Backend/src/main/java/com/NetworkTicketing/NetworkTicketing/NetworkTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.NetworkTicketing.NetworkTicketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkTicketingApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/NetworkTicketing/NetworkTicketing/NetworkTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NetworkTicketing.NetworkTicketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
